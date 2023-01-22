838ba2bedc5ab5203b11cfff07649900d66993a9
MaximShly <mvshlyaptsev@gmail.com>
2023-01-22 15:08:42 -0500

    12

--- ball.js
+++ ball.js
@@ -40,8 +40,6 @@ ball2.style.top = positionY + "px";
     positionY === Ymin
   ) {
     reverse = !reverse;
-setInterval(moveBall, xyz - 100);
+setInterval(moveBall, (xyz = xyz - 100);
   }
 }
-
-setInterval(moveBall, xyz);
